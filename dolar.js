4f7a21c0819b1649d0d5959c445899fde5f5efdd
moisesneto96 <moisesfrifas7@gmail.com>
2023-03-22 10:59:45 -0300

    Conversor

---
+++ dolar.js
@@ -0,0 +1,7 @@
+function calcularDolar(){
+    let real = document.getElementById("input-real").value
+    let dolar= document.getElementById("input-dolar").value
+
+    let resultado = real / dolar
+    alert(resultado)
+}
